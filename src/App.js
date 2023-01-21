5f2418f9ff7fbe5c92eeac91ce25601112b0e2af
venkatasubbamma990 <venkatasubbamma990@email.com>
2023-01-22 00:25:19 +0530

    dream is added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import DreamProject from './Component/Dream-project';
+
+function App() {
+  return (
+    <div className="App">
+      <DreamProject projectname="Social Media App"/>
+      
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Dream-project.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function DreamProject({projectname}){
+   
+    return(
+        <div>
+        <h1> My Dream Project is : <span>"{projectname}"</span>
+            </h1>
+        <p> my Dream project is to develop Social Media App which can provide access to make reels,to upload stories,pictures,and to connect with friends. my Dream project is to develop Social Media App which can provide access to make reels,to upload stories,pictures,and to connect with friends. </p>
+    </div>
+    )
+}
+export default DreamProject;
